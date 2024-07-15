7c0d9086cab340ca8dfae852fc0b9fdd4c940764
Drazer Walyley <onksssss4test@gmail.com>
2024-07-15 20:11:56 +0000

    moveing file

---
+++ models/example/myairbtransform.sql
@@ -0,0 +1,26 @@
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+    After pull
+*/
+
+{{ config(materialized='table') }}
+
+with above30emp as (
+
+   select * from employee
+   where age > 30
+
+)
+
+select *
+from above30emp
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
